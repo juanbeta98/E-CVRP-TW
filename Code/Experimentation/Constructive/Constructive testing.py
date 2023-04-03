199f3d537b725889e03e2b014bf17403a81fe46a
juanbeta98 <juan.beta98@gmail.com>
2023-04-03 09:31:55 -0500

    Prepare testing in windows computer

--- Code/Experimentation/Constructive/Constructive testing.py
+++ Code/Experimentation/Constructive/Constructive testing.py
@@ -5,8 +5,8 @@ import sys
 import pickle
 import matplotlib.pyplot as plt
 
-path: str = '/Users/juanbeta/My Drive/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
-#path: str = 'C:/Users/jm.betancourt/Documents/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
+#path: str = '/Users/juanbeta/My Drive/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
+path: str = 'C:/Users/jm.betancourt/Documents/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
 
 sys.path.insert(0,path)
 from E_CVRP_TW import  E_CVRP_TW, Constructive, Experiment
@@ -47,14 +47,27 @@ colors: list = ['blue', 'red', 'black', 'purple', 'green', 'orange']
 '''
 Instance testing
 '''
-max_time: int = 300 # 5 minutes
+max_time: int = 60*5 # 5 minutes
 test_bed = env.sizes['s'][:2] + env.sizes['m'][:2] + env.sizes['l'][:3]
 
-for instance in test_bed:
+for instance in env.instances:
     # Saving performance
     Results = {}
     Incumbents = []
     Times = []
+
+    if instance in env.sizes['s']:  
+        max_time = 60*2
+        RCL_alpha = 0.5
+
+    elif instance in env.sizes['m']:   
+        max_time = 60*4
+        RCL_alpha = 0.25
+
+    else:   
+        max_time = 60*8
+        RCL_alpha = 0.15
+
 
     if verbose: 
         print(f'\n\n################# Instance {instance} #################')
