b100eb9023cb621ebfae13aa29872655d80cf2bd
juanbeta98 <juan.beta98@gmail.com>
2023-04-02 22:44:28 -0500

    Differentiate retrieve/plot performance functions

--- Code/Experimentation/plot_performance.py
+++ Code/Experimentation/plot_performance.py
@@ -2,13 +2,17 @@ import pickle
 import matplotlib.pyplot as plt
 import sys
 
-def plot_const_performance(instance, path):
-
+def retrieve_const_performance(instance, path):
     path += 'Constructive/'
 
     file = open(path + f'results_{instance}', 'rb')
     data = pickle.load(file)
     file.close()
+
+    return data
+
+
+def plot_const_performance(data):
 
     plt.plot(data['inc times'], data['incumbents'], color = 'purple')
     plt.title(f"Constructive's performance {instance}")
