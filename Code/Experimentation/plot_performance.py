85208aeca82133e01f74b3f778b878fed41747f4
juanbeta98 <juan.beta98@gmail.com>
2023-04-09 21:50:15 -0500

    Add legend to graphs

--- Code/Experimentation/plot_performance.py
+++ Code/Experimentation/plot_performance.py
@@ -1,5 +1,6 @@
 import pickle
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import sys
 
 def retrieve_const_performance(instance, path):
@@ -12,11 +13,20 @@ def retrieve_const_performance(instance, path):
 def plot_const_performance(data, instance, testing = 'Constructive'):
     colors = ['red', 'orange', 'brown', 'green', 'purple' , 'blue' ,'black', 'pink', ]
     i = 0
+    legend_elements = []
     for key, value in data.items():
-        plt.plot(value['inc times'], value['incumbents'], color = colors[i]);i+=1
+        plt.plot(value['inc times'], value['incumbents'], color = colors[i])
+        legend_elements.append(Line2D([0], [0], marker='_', color=colors[i], label=str(key), lw=0,
+                          markerfacecolor=colors[i], markersize=8))
+        i+=1
+
     plt.title(f"{testing}'s performance {instance}")
     plt.xlabel('Time (s)')
     plt.ylabel('Incumbent')
+
+    ax = plt.gca()
+    ax.legend(handles=legend_elements, loc='upper right', fontsize=8)
+    
     plt.show()
 
 
