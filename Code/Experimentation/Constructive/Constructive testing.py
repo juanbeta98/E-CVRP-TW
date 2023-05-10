279b7a679fd363fc8f0eff77ba2e09fdb1cfcd7e
Juan <juan.beta98@gmail.com>
2023-05-10 05:53:45 -0500

    Update on Constructive testing test_bed - Update files for distance RCL crit testing

--- Code/Experimentation/Constructive/Constructive testing.py
+++ Code/Experimentation/Constructive/Constructive testing.py
@@ -4,8 +4,8 @@ import sys
 import pickle
 import matplotlib.pyplot as plt
 
-path: str = '/Users/juanbeta/My Drive/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
-# path: str = 'C:/Users/jm.betancourt/Documents/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
+# path: str = '/Users/juanbeta/My Drive/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
+path: str = 'C:/Users/jm.betancourt/Documents/Research/Energy/E-CVRP-TW/Code/' ##### CHANGE WHEN NECESSARY!!!
 
 sys.path.insert(0,path)
 from E_CVRP_TW import  E_CVRP_TW, Constructive, Experiment
@@ -34,7 +34,7 @@ RCL_alpha_list: list[float] = [0.15, 0.25, 0.35, 0.5]
 training_prop = 0.5
 constructive: Constructive = Constructive()
 
-RCL_criterion: str = 'TimeWindow'
+RCL_criterion: str = 'distance'
 
 '''
 EXPERIMENTATION
@@ -52,10 +52,12 @@ Instance testing
 # test_bed = env.sizes['s']+env.sizes['m']
 # test_bed = env.sizes['l']
 
-# test_bed = env.sizes['l'][:int(len(env.sizes['l']/2))]
-# test_bed = env.sizes['l'][int(len(env.sizes['l']/2)):]
+# test_bed = env.sizes['l'][:int(len(env.sizes['l'])/2)]
+# test_bed = env.sizes['l'][int(len(env.sizes['l'])/2):]
 
-test_bed = ['c103C5.txt']
+# test_bed = env.sizes['l'][:int(len(env.sizes['l'])/3)]
+# test_bed = env.sizes['l'][int(len(env.sizes['l'])/3):2*int(len(env.sizes['l'])/3)]
+test_bed = env.sizes['l'][2*int(len(env.sizes['l'])/3):]
 
 for instance in test_bed:
     # Saving performance
