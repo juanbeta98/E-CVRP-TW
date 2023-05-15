94e3342ad0d38c7c60e326ff3c10291ed5d34dcb
juanbeta98 <juan.beta98@gmail.com>
2023-05-15 09:03:34 -0500

    Fix bug on evaluated insertion - 'visited costumers:'

--- Code/E_CVRP_TW.py
+++ Code/E_CVRP_TW.py
@@ -1093,7 +1093,7 @@ class Genetic():
 
         rank_index:list = self.rank_indexes(eff_rates)
 
-        if config['criterion'] == 'visited costumers:':     worst_route_index = n_visited_c.index(min(n_visited_c))
+        if config['criterion'] == 'visited costumers':     worst_route_index = n_visited_c.index(min(n_visited_c))
         elif config['criterion'] == 'phi rate':             worst_route_index = rank_index.index(max(rank_index))
         elif config['criterion'] == 'Hybrid':               worst_route_index = choice([rank_index.index(max(rank_index)),n_visited_c.index(min(n_visited_c))])
         
