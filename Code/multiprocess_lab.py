7d9345bd69b553452fe7b6f04d76192b396d5076
juanbeta98 <juan.beta98@gmail.com>
2023-06-06 00:12:50 -0500

    V1.0 Testing

--- Code/multiprocess_lab.py
+++ Code/multiprocess_lab.py
@@ -40,7 +40,7 @@ test_batch = env.instances
 
 if __name__ == '__main__':
     for num, Configs in enumerate(Grid): 
-        os.mkdir(path + f'Exp {num}')
+        os.mkdir(saving_path + f'Exp {num}')
         with open(saving_path + f'Exp {num}/readme.txt', 'w') as f:
             readme = f'Experiment {num}'
             readme += f'\nDarwinian phi rate: \t{Configs["Darwinian phi rate"]["penalization"]} - {Configs["Darwinian phi rate"]["length restriction"]}'
